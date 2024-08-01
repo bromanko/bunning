be9f38da653d1e4b0e87fc6625498a675c7087d7
Brian Romanko <hello@bromanko.com>
2024-08-01 15:43:59 -0400

    Render html via JSX

--- apps/web/routes/root.tsx
+++ apps/web/routes/root.tsx
@@ -1,7 +1,35 @@
 import { Hono } from "hono";
+import type { FC } from "hono/jsx";
+
+const Layout: FC = ({ children }) => {
+	return (
+		<html lang="en-US">
+			<head>TEST</head>
+			<body>{children}</body>
+		</html>
+	);
+};
+
+const Top: FC<{ messages: string[] }> = (props: {
+	messages: string[];
+}) => {
+	return (
+		<Layout>
+			<h1>Hello Hono!</h1>
+			<ul>
+				{props.messages.map((message) => {
+					return <li key={message}>{message}!!</li>;
+				})}
+			</ul>
+		</Layout>
+	);
+};
 
 const app = new Hono();
 
-app.get("/", (c) => c.text("Hello Bun!"));
+app.get("/", (c) => {
+	const messages = ["Good Morning", "Good Evening", "Good Night"];
+	return c.html(<Top messages={messages} />);
+});
 
 export default app;
