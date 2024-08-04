c98d6a0134f9941a831b3257ecf51d56e4dbed81
Brian Romanko <hello@bromanko.com>
2024-08-04 16:10:40 -0400

    Add concurrent build of css and bun

--- apps/web/src/routes/root.tsx
+++ apps/web/src/routes/root.tsx
@@ -8,7 +8,7 @@ const Layout: FC = ({ children }) => {
 				<title>Test</title>
 				<link rel="stylesheet" href="/static/styles.css" />
 			</head>
-			<body class="bg-red-100">{children}</body>
+			<body class="bg-gray-500">{children}</body>
 		</html>
 	);
 };
--- apps/web/src/server.ts
+++ apps/web/src/server.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import { withHtmlLiveReload } from "bun-html-live-reload";
+import path from "node:path";
 import root from "./routes/root";
 import st from "./routes/static";
 
@@ -8,10 +9,15 @@ const mkServer = (port: number) => {
 	app.route("/", root);
 	app.route("/static/", st);
 
-	return withHtmlLiveReload({
-		port,
-		fetch: app.fetch,
-	});
+	return withHtmlLiveReload(
+		{
+			port,
+			fetch: app.fetch,
+		},
+		{
+			watchPath: path.resolve(import.meta.dir),
+		},
+	);
 };
 
 export default mkServer;
