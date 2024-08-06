2a57bdd7a8e3330986e2986f2a97dba0539638bf
Brian Romanko <hello@bromanko.com>
2024-08-06 14:15:43 -0500

    Add new ContextRenderer for providing page title

--- apps/web/src/components/layouts.tsx
+++ apps/web/src/components/layouts.tsx
@@ -1,5 +1,12 @@
 import type { FC } from "hono/jsx";
 
+declare module "hono" {
+	interface ContextRenderer {
+		// biome-ignore lint/style/useShorthandFunctionType: Override default ContextRenderer
+		(content: string | Promise<string>, props: { title: string }): Response;
+	}
+}
+
 export const HtmlBase: FC = ({ title = "Bunning", children }) => {
 	return (
 		<html lang="en-US">
--- apps/web/src/routes/root.tsx
+++ apps/web/src/routes/root.tsx
@@ -1,39 +1,35 @@
 import { Hono } from "hono";
 import type { FC } from "hono/jsx";
-import { jsxRenderer, useRequestContext } from "hono/jsx-renderer";
+import { jsxRenderer } from "hono/jsx-renderer";
 import { HtmlBase, Layout } from "../components/layouts";
 
 const Top: FC<{ messages: string[] }> = (props: {
 	messages: string[];
-}) => {
-	return (
-		<>
-			<h1>Hello Hono!</h1>
-			<ul>
-				{props.messages.map((message) => {
-					return <li key={message}>{message}!!</li>;
-				})}
-			</ul>
-		</>
-	);
-};
+}) => (
+	<>
+		<h1>Hello Hono!</h1>
+		<ul>
+			{props.messages.map((message) => {
+				return <li key={message}>{message}!!</li>;
+			})}
+		</ul>
+	</>
+);
 
 const app = new Hono();
 
 app.use(
 	"*",
-	jsxRenderer(({ children }) => {
-		return (
-			<HtmlBase>
-				<Layout>{children}</Layout>
-			</HtmlBase>
-		);
-	}),
+	jsxRenderer(({ title, children }) => (
+		<HtmlBase title={title}>
+			<Layout>{children}</Layout>
+		</HtmlBase>
+	)),
 );
 
 app.get("/", (c) => {
 	const messages = ["Good Morning", "Good Morrow", "Good Night"];
-	return c.render(<Top messages={messages} />);
+	return c.render(<Top messages={messages} />, { title: "Hello Hono" });
 });
 
 export default app;
