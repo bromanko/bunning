021e93e69e3a61340dcfcde5808cd95d4022a198
Brian Romanko <hello@bromanko.com>
2024-08-17 12:00:30 -0700

    Convert json to lower_snake_case

--- dotnet/src/Bunning.MailCtl/HttpRequester.fs
+++ dotnet/src/Bunning.MailCtl/HttpRequester.fs
@@ -1,11 +1,12 @@
 namespace Bunning.MailCtl
 
+open System.Net.Http
+open System.Text.Json
 open System.Text.Json.Serialization
 open System.Threading
 open System.Threading.Tasks
 open FsHttp
 open FsToolkit.ErrorHandling.Operator.Task
-open FSharp.SystemTextJson
 
 type ApiConfig = { Endpoint: string; ApiKey: string }
 
@@ -14,7 +15,15 @@ type IHttpRequester =
     abstract member getRequest<'R>: cfg: ApiConfig -> Task<'R>
 
 type HttpRequester() =
-    let jsonSerializerOptions = JsonFSharpOptions.Default().ToJsonSerializerOptions()
+    let jsonSerializerOptions () =
+        let jOpts = JsonSerializerOptions(PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower)
+        JsonFSharpOptions.Default().AddToJsonSerializerOptions(jOpts)
+        jOpts
+
+    let logBody (req: HttpRequestMessage) =
+        let s = req.Content.ReadAsStringAsync() |> Async.AwaitTask |> Async.RunSynchronously
+        printfn $"%A{s}"
+        req
 
     interface IHttpRequester with
         member this.postRequest<'T, 'R>(cfg: ApiConfig) (data: 'T) =
@@ -25,7 +34,8 @@ type HttpRequester() =
                 CacheControl "no-cache"
                 body
                 ContentType "application/json"
-                jsonSerializeWith jsonSerializerOptions data
+                jsonSerializeWith (jsonSerializerOptions()) data
+                config_transformHttpRequestMessage logBody
             }
             |> Request.sendTAsync
             >>= Response.deserializeJsonTAsync<'R>(CancellationToken.None)
